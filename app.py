3fe2d0ba8f87930f4623b5cedd28d6997b281b45
WajidKarimm <karimwajid0101@gmail.com>
2025-05-21 12:01:47 +0500

    changes

--- app.py
+++ app.py
@@ -5,10 +5,10 @@ def substraction(a, b):
     return a - b
 
 
-def division(a, b):
+def division(a, b,c):
     return a / b
 
-def multiplication(a, b):
+def multiplication(a, b,c):
     return a * b
 
 
